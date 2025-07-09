599087fef8397f76347ab695fe67c56f50fcdffe
deeje <deeje@mac.com>
2025-07-09 14:24:30 -0700

    add support for macOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ let package = Package(
     name: "CloudCore",
     platforms: [
         .iOS(.v15),
-        .watchOS(.v8),
+        .macOS(.v12),
+        .watchOS(.v8)
     ],
     products: [
         .library(name: "CloudCore", targets: ["CloudCore"])
